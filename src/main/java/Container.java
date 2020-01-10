82cccdfdf57a6019358daf3df84fe8fa219db4ee
Li Weinan <li.weinan@rea-group.com>
2020-01-10 14:54:42 +0800

    empty message

--- src/main/java/Container.java
+++ src/main/java/Container.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.*;
 
-@SuppressWarnings("raw")
+@SuppressWarnings("rawtypes")
 public class Container {
 
   private Set<Object> beans = new HashSet<>();
